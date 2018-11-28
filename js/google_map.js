be05c8d8de7012d5a51cab58f40d9737d294e283
sharpsean <44674778+sharpsean@users.noreply.github.com>
2018-11-28 02:58:11 -0600

    Update google_map.js

--- js/google_map.js
+++ js/google_map.js
@@ -29,16 +29,8 @@ function init() {
     // Create the Google Map using out element and options defined above
     var map = new google.maps.Map(mapElement, mapOptions);
     
-    var addresses = ['Brooklyn'];
 
-    for (var x = 0; x < addresses.length; x++) {
-        $.getJSON('http://maps.googleapis.com/maps/api/geocode/json?address='+addresses[x]+'&sensor=false', null, function (data) {
-            var p = data.results[0].geometry.location
-            var latlng = new google.maps.LatLng(p.lat, p.lng);
-            new google.maps.Marker({
-                position: latlng,
-                map: map,
-                icon: 'images/loc.png'
+
             });
 
         });
