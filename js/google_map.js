c0cd48ccae7a878683ce536d642cfc81b7f4b9da
sharpsean <44674778+sharpsean@users.noreply.github.com>
2018-12-17 12:27:30 -0600

    Update google_map.js

--- js/google_map.js
+++ js/google_map.js
@@ -5,13 +5,13 @@ function init() {
     // Basic options for a simple Google Map
     // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
     // var myLatlng = new google.maps.LatLng(40.71751, -73.990922);
-    var myLatlng = new google.maps.LatLng(41.882174, -87.630666);
+    var myLatlng = new google.maps.LatLng(41.877615, -87.625710);
     // 39.399872
     // -8.224454
              
     var mapOptions = {
         // How zoomed in you want the map to start at (always required)
-        zoom: 12,
+        zoom: 13,
 
         // The latitude and longitude to center the map (always required)
         center: myLatlng,
@@ -38,4 +38,3 @@ function init() {
     }
     
 google.maps.event.addDomListener(window, 'load', init);
-marker.setMap(map);
